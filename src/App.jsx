440af10726d4e71fb152e284b518358e99d8b972
waris_sayed <warissayed72@gmail.com>
2024-07-24 15:20:21 +0530

    "updating ui "

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,46 @@
-import { useState } from 'react'
-import './App.css'
-import Todos from './components/Todos'
-import AddTodo from './components/AddTodo'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentDateTime, setCurrentDateTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentDateTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+  const getDayName = (date) => {
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    return days[date.getDay()];
+  };
 
   return (
     <>
-    <h1>Learn about redux toolkit</h1>
-    <AddTodo />
-    <Todos />
-  </>
-  )
+      <div className="flex items-center justify-center gap-3">
+        <h1 className="text-base font-bold">
+          {currentDateTime.toLocaleDateString()}
+        </h1>
+        <h1 className="text-base font-bold">{getDayName(currentDateTime)}</h1>
+        <h1 className="text-base font-bold">
+          {currentDateTime.toLocaleTimeString()}
+        </h1>
+      </div>
+      <AddTodo />
+      <Todos />
+    </>
+  );
 }
 
-export default App
+export default App;
